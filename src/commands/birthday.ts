28e7183916dce4345957f8e18885ca51d8de4d42
Justin Chu <support@justinchu.com>
2021-12-22 08:08:02 -0800

    Add Birthday Command

--- src/commands/birthday.ts
+++ src/commands/birthday.ts
@@ -13,11 +13,28 @@ import {
 
 @Discord()
 abstract class AppDiscord {
-    @Slash("hello")
-    hello(
-      @SlashOption("user", { type: "USER" }) user: GuildMember | User,
+    @Slash("add-birthdate")
+    add(
+      @SlashOption("month", { 
+        required: true,
+        type: "STRING",
+        description:"Month of your birthday (MM)"
+      }) month: number,
+      @SlashOption("day", { 
+        required: true,
+        type: "STRING",
+        description:"Day of your birthday (DD)"
+      }) day: number,
+      @SlashOption("year", { 
+        required: true,
+        type: "STRING",
+        description:"Year of your birthday (YYYY)"
+      }) year: number,
       interaction: CommandInteraction
-    ): void {
-      interaction.reply(`${user}`);
+    ) {
+      let user = interaction.user;
+
+      interaction.reply(`Hello there ${user.username}! I've recorded your birthday as ${month}/${day}/${year}`);
+
     }  
 }
